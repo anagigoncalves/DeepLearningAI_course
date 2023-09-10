86d28d9b28d49536fb216d1e2507ad83be977dd9
AnaG OpenLab <ana.gigoncalves@gmail.com>
2023-09-10 22:52:56 +0100

    sequence models course

---
+++ emojify_v1.py
@@ -0,0 +1,134 @@
+import numpy as np
+from emo_utils import *
+import emoji
+import matplotlib.pyplot as plt
+from test_utils import *
+
+X_train, Y_train = read_csv('data/train_emoji.csv')
+X_test, Y_test = read_csv('data/tesss.csv')
+
+maxLen = len(max(X_train, key=lambda x: len(x.split())).split())
+
+Y_oh_train = convert_to_one_hot(Y_train, C = 5)
+Y_oh_test = convert_to_one_hot(Y_test, C = 5)
+
+word_to_index, index_to_word, word_to_vec_map = read_glove_vecs('data/glove.6B.50d.txt')
+
+def sentence_to_avg(sentence, word_to_vec_map):
+    """
+    Converts a sentence (string) into a list of words (strings). Extracts the GloVe representation of each word
+    and averages its value into a single vector encoding the meaning of the sentence.
+    
+    Arguments:
+    sentence -- string, one training example from X
+    word_to_vec_map -- dictionary mapping every word in a vocabulary into its 50-dimensional vector representation
+    
+    Returns:
+    avg -- average vector encoding information about the sentence, numpy-array of shape (J,), where J can be any number
+    """
+    # Get a valid word contained in the word_to_vec_map. 
+    any_word = list(word_to_vec_map.keys())[0]
+    
+    # Step 1: Split sentence into list of lower case words (≈ 1 line)
+    words = [word.lower() for word in sentence.split()]
+
+    # Initialize the average word vector, should have the same shape as your word vectors.
+    # Use `np.zeros` and pass in the argument of any word's word 2 vec's shape
+    avg = np.zeros(np.shape(word_to_vec_map[any_word]))
+    
+    # Initialize count to 0
+    count = 0
+    
+    # Step 2: average the word vectors. You can loop over the words in the list "words".
+    for w in words:
+        # Check that word exists in word_to_vec_map
+        if w in word_to_vec_map:
+            avg += word_to_vec_map[w]
+            # Increment count
+            count +=1
+          
+    if count > 0:
+        # Get the average. But only if count > 0
+        avg = avg/count
+    return avg
+
+def model(X, Y, word_to_vec_map, learning_rate = 0.01, num_iterations = 400):
+    """
+    Model to train word vector representations in numpy.
+    
+    Arguments:
+    X -- input data, numpy array of sentences as strings, of shape (m,)
+    Y -- labels, numpy array of integers between 0 and 7, numpy-array of shape (m, 1)
+    word_to_vec_map -- dictionary mapping every word in a vocabulary into its 50-dimensional vector representation
+    learning_rate -- learning_rate for the stochastic gradient descent algorithm
+    num_iterations -- number of iterations
+    
+    Returns:
+    pred -- vector of predictions, numpy-array of shape (m, 1)
+    W -- weight matrix of the softmax layer, of shape (n_y, n_h)
+    b -- bias of the softmax layer, of shape (n_y,)
+    """
+    
+    # Get a valid word contained in the word_to_vec_map 
+    any_word = list(word_to_vec_map.keys())[0]
+        
+    # Define number of training examples
+    m = Y.shape[0]                             # number of training examples
+    n_y = len(np.unique(Y))                    # number of classes  
+    n_h = word_to_vec_map[any_word].shape[0]   # dimensions of the GloVe vectors 
+    
+    # Initialize parameters using Xavier initialization
+    W = np.random.randn(n_y, n_h) / np.sqrt(n_h)
+    b = np.zeros((n_y,))
+    
+    # Convert Y to Y_onehot with n_y classes
+    Y_oh = convert_to_one_hot(Y, C = n_y) 
+    
+    # Optimization loop
+    for t in range(num_iterations): # Loop over the number of iterations
+        
+        cost = 0
+        dW = 0
+        db = 0
+        
+        for i in range(m):          # Loop over the training examples
+            
+            # Average the word vectors of the words from the i'th training example
+            avg = sentence_to_avg(X[i], word_to_vec_map)
+
+            # Forward propagate the avg through the softmax layer. 
+            # You can use np.dot() to perform the multiplication.
+            z = np.dot(W, avg)+b
+            a = softmax(z)
+
+            # Add the cost using the i'th training label's one hot representation and "A" (the output of the softmax)
+            cost += -np.squeeze(np.sum(Y_oh[i]*np.log(a)))
+            
+            # Compute gradients 
+            dz = a - Y_oh[i]
+            dW += np.dot(dz.reshape(n_y,1), avg.reshape(1, n_h))
+            db += dz
+
+            # Update parameters with Stochastic Gradient Descent
+            W = W - learning_rate * dW
+            b = b - learning_rate * db
+            
+        assert type(cost) == np.float64, "Incorrect implementation of cost"
+        assert cost.shape == (), "Incorrect implementation of cost"
+        
+        if t % 100 == 0:
+            print("Epoch: " + str(t) + " --- cost = " + str(cost))
+            pred = predict(X, Y, W, b, word_to_vec_map) #predict is defined in emo_utils.py
+    return pred, W, b
+
+np.random.seed(1)
+pred, W, b = model(X_train, Y_train, word_to_vec_map)
+print("Training set:")
+pred_train = predict(X_train, Y_train, W, b, word_to_vec_map)
+print('Test set:')
+pred_test = predict(X_test, Y_test, W, b, word_to_vec_map)
+X_my_sentences = np.array(["i treasure you", "i love you", "funny lol", "lets play with a ball", "food is ready", "today is not good"])
+Y_my_labels = np.array([[0], [0], [2], [1], [4],[3]])
+
+pred = predict(X_my_sentences, Y_my_labels , W, b, word_to_vec_map)
+print_predictions(X_my_sentences, pred)
